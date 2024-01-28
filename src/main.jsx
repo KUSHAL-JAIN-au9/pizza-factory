981e08b1a74eabc894b3f828b3c04fe8cd38daee
Kushal Jain <jkushal069@gmail.com>
2024-01-29 00:13:28 +0530

    fixed build error

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import PizzaForm from "./components/pizzaForm.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/order", element: <PizzaForm /> },
-]);
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
